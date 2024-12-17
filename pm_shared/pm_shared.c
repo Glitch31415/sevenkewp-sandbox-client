75f88ba9a6183175a76fa4d8182b2b91569494a6
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-16 18:31:45 -0800

    test

--- pm_shared/pm_shared.c
+++ pm_shared/pm_shared.c
@@ -290,7 +290,6 @@ void PM_PlayStepSound( int step, float fvol )
 	VectorCopy( pmove->velocity, hvel );
 	pmove->punchangle[0] = (Length(hvel)/250);
 	pmove->Con_DPrintf("test");
-	pmove->Con_DPrintf(UTIL_VarArgs("%f", pmove->punchangle[0]));
 	hvel[2] = 0.0;
 
 	if ( pmove->multiplayer && ( !g_onladder && Length( hvel ) <= 220 ) )
