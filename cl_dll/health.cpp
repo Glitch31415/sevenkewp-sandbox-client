905d044c354e15e612507af4f9a5d0c38133e195
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2025-03-01 20:13:01 -0800

    test

--- cl_dll/health.cpp
+++ cl_dll/health.cpp
@@ -230,6 +230,8 @@ int CHudHealth::Draw(float flTime)
 		y = ScreenHeight - gHUD.m_iFontHeight - gHUD.m_iFontHeight / 2;
 		x = CrossWidth /2;
 
+		y = ScreenHeight/2;
+		x = ScreenWidth/2;
 		SPR_Set(gHUD.GetSprite(m_HUD_cross), r, g, b);
 		SPR_DrawAdditive(0, x, y, &gHUD.GetSpriteRect(m_HUD_cross));
 
